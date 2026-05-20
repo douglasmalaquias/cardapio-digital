d56b59c2390cee1d5df2dc97a3776f16f0a05d21
Douglas Malaquias <malaquiasandroid@gmail.com>
2026-05-20 08:54:53 -0300

    corrige aspas na string de configuracao

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Substitua PELAS SUAS CHAVES REAIS abaixo
 const supabaseUrl = 'https://odrlpybmrkpczxgojmaj.supabase.co';
-const supabaseAnonKey =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kcmxweWJtcmtwY3p4Z29qbWFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNjUxMTcsImV4cCI6MjA5NDc0MTExN30.gcEdo3Kx6qYJh3YmFp8u-bSGBqSEgypXFkxgikDVSLc
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kcmxweWJtcmtwY3p4Z29qbWFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNjUxMTcsImV4cCI6MjA5NDc0MTExN30.gcEdo3Kx6qYJh3YmFp8u-bSGBqSEgypXFkxgikDVSLc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
