c489ea8e4fb4870760a7be1dc70fa6bab9765178
Douglas Malaquias <malaquiasandroid@gmail.com>
2026-05-20 13:37:00 -0300

    fix: remocao definitiva de redirecionamento e atualizacao das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Rota Inicial / Hub Central da Plataforma */}
         <Route path="/" element={<Home />} />
-
-        {/* Rota dinâmica do cliente tablet: aceita qualquer slug de restaurante */}
         <Route path="/:slug" element={<CustomerView />} />
-
-        {/* Rotas de administração do lojista e anúncios */}
         <Route path="/admin/:slug/produtos" element={<AdminView />} />
         <Route path="/admin/:slug/anuncios" element={<AdminAdsView />} />
       </Routes>
