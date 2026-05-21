8cac6ce7e4f4cddfc1fc9d1c529a5f51db743d2e
Malaquias <64491369+douglasmalaquias@users.noreply.github.com>
2026-05-21 14:17:44 -0300

    Add routes for AdminProdutos and AdminAnuncios
    atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CustomerView from './pages/CustomerView';
 import AdminEstabelecimentos from './pages/AdminEstabelecimentos';
-import AdminProdutos from './pages/AdminProdutos'; // <-- ADICIONADO AQUI
+import AdminProdutos from './pages/AdminProdutos';
+import AdminAnuncios from './pages/AdminAnuncios'; // <-- ADICIONADO AQUI
 import Login from './pages/Login';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        {/* Rota Pública do Hub Inicial */}
         <Route path="/" element={<Home />} />
-        
-        {/* Rota de Autenticação */}
         <Route path="/login" element={<Login />} />
-        
-        {/* Rota de Gestão de Clientes SaaS (Sua) */}
         <Route path="/admin/clientes" element={<AdminEstabelecimentos />} />
+        <Route path="/admin/:slug/produtos" element={<AdminProdutos />} />
         
-        {/* Rota de Gestão de Produtos (Do Lojista) */}
-        <Route path="/admin/:slug/produtos" element={<AdminProdutos />} /> {/* <-- ADICIONADO AQUI */}
+        {/* Nova rota de anúncios */}
+        <Route path="/admin/:slug/anuncios" element={<AdminAnuncios />} /> 
         
-        {/* Rota de Visualização do Cardápio Público */}
         <Route path="/:slug" element={<CustomerView />} />
       </Routes>
     </Router>
