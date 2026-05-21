924bb1ed4a88c17fbd49060508724b8d32ab9550
Malaquias <64491369+douglasmalaquias@users.noreply.github.com>
2026-05-21 10:58:37 -0300

    Wrap routes in Router componentadd router
    Wrap routes in Router componentadd router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CustomerView from './pages/CustomerView';
 import AdminEstabelecimentos from './pages/AdminEstabelecimentos';
-import Login from './pages/Login'; // <-- ADICIONADO AQUI
+import Login from './pages/Login';
 
 export default function App() {
   return (
-    <Routes>
-      {/* Rota Pública do Hub Inicial */}
-      <Route path="/" element={<Home />} />
-      
-      {/* Rota de Autenticação */}
-      <Route path="/login" element={<Login />} /> {/* <-- ADICIONADO AQUI */}
-      
-      {/* Rota de Gestão de Clientes SaaS */}
-      <Route path="/admin/clientes" element={<AdminEstabelecimentos />} />
-      
-      {/* Rota de Visualização do Cardápio Público */}
-      <Route path="/:slug" element={<CustomerView />} />
-    </Routes>
+    <Router>
+      <Routes>
+        {/* Rota Pública do Hub Inicial */}
+        <Route path="/" element={<Home />} />
+        
+        {/* Rota de Autenticação */}
+        <Route path="/login" element={<Login />} />
+        
+        {/* Rota de Gestão de Clientes SaaS */}
+        <Route path="/admin/clientes" element={<AdminEstabelecimentos />} />
+        
+        {/* Rota de Visualização do Cardápio Público */}
+        <Route path="/:slug" element={<CustomerView />} />
+      </Routes>
+    </Router>
   );
 }
