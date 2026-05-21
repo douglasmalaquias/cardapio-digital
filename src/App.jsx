f7709f9977011a90cd086f618be850aa9195542a
Malaquias <64491369+douglasmalaquias@users.noreply.github.com>
2026-05-21 07:51:18 -0300

    route estabelecimentos
    route estabelecimentos

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import CustomerView from './pages/CustomerView';
 import AdminView from './pages/AdminView';
 import AdminAdsView from './pages/AdminAdsView';
+import AdminEstabelecimentos from './pages/AdminEstabelecimentos';
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/:slug" element={<CustomerView />} />
         <Route path="/admin/:slug/produtos" element={<AdminView />} />
         <Route path="/admin/:slug/anuncios" element={<AdminAdsView />} />
+        <Route path="/admin/clientes" element={<AdminEstabelecimentos />} />
       </Routes>
     </BrowserRouter>
   );
