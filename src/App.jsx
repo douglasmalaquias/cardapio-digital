f511a7daf00cd4683fd943f2205f90500cd7f9e4
Malaquias <64491369+douglasmalaquias@users.noreply.github.com>
2026-05-21 13:58:57 -0300

    AdminProdutos route to App component
    rote produtos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CustomerView from './pages/CustomerView';
 import AdminEstabelecimentos from './pages/AdminEstabelecimentos';
+import AdminProdutos from './pages/AdminProdutos'; // <-- ADICIONADO AQUI
 import Login from './pages/Login';
 
 export default function App() {
@@ -15,8 +16,11 @@ export default function App() {
         {/* Rota de Autenticação */}
         <Route path="/login" element={<Login />} />
         
-        {/* Rota de Gestão de Clientes SaaS */}
+        {/* Rota de Gestão de Clientes SaaS (Sua) */}
         <Route path="/admin/clientes" element={<AdminEstabelecimentos />} />
+        
+        {/* Rota de Gestão de Produtos (Do Lojista) */}
+        <Route path="/admin/:slug/produtos" element={<AdminProdutos />} /> {/* <-- ADICIONADO AQUI */}
         
         {/* Rota de Visualização do Cardápio Público */}
         <Route path="/:slug" element={<CustomerView />} />
